d932d89ba92311c847b90ee35e19d16e6d106e3b
Mariia Posselt <maruw@ya.ru>
2024-01-12 11:38:12 +0100

    solve challenge

--- js/index.js
+++ js/index.js
@@ -9,3 +9,40 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+const secondTeaser = document.createElement("article");
+
+secondTeaser.classList.add("product");
+
+secondTeaser.innerHTML = `
+<div class="product__body">
+        <div class="product__text-container">
+          <h2 class="product__name">${name}</h2>
+          <ul class="product__categories">
+            <li class="product__category">${category1}</li>
+            <li class="product__category">${category2}</li>
+            <li class="product__category">${category3}</li>
+          </ul>
+          <p class="product__description">
+            ${description}
+          </p>
+        </div>
+        <div class="product__image-container">
+          <img
+            class="product__image"
+            src=${imageSrc}
+          />
+        </div>
+</div>
+<footer class="product__footer">
+        <span class="product__price">${price}</span>
+        <button type="button" class="product__buy-button" data-js="button">Buy</button>
+</footer>`;
+
+document.body.append(secondTeaser);
+
+const buyButton2 = document.querySelector('[data-js="button"]');
+
+buyButton2.addEventListener("click", () => {
+  console.log(name, price);
+});
