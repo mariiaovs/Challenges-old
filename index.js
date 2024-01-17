8a6e2f4e6534d88f38a81fcaa5c4089170d73313
Mariia Posselt <maruw@ya.ru>
2024-01-17 11:32:47 +0100

    solve challenge

--- index.js
+++ index.js
@@ -23,13 +23,17 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => card.id === "2");
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter((card) => card.tags.length === 3);
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter((card) => !card.isBookmarked);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter(
+  (card) =>
+    card.isBookmarked &&
+    (card.tags.includes("html") || card.tags.includes("js"))
+);
 
 export {
   onlyCardWithIdTwo,
