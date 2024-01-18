7a0f486b4d334f10ad2ffdd10d41db6fb40b1bbd
Mariia Posselt <maruw@ya.ru>
2024-01-18 11:33:49 +0100

    data from form

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function generateEmail(user) {
-  return `${user.firstName}.${user.lastName}@example.com`.toLowerCase();
+  return `${user.firstName.trim()}.${user.lastName.trim()}@example.com`.toLowerCase();
 }
 
 const newUser = {
@@ -9,3 +9,15 @@ const newUser = {
 
 const email = generateEmail(newUser);
 console.log(email);
+
+const form = document.querySelector('[data-js="form"]');
+const span = document.querySelector('[data-js="generated-email"]');
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+  span.textContent = generateEmail(data);
+  e.target.reset();
+  e.target.firstName.focus();
+});
