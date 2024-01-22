47cb9bfe1b92b6062194841d499a4f2023656b79
Mariia Posselt <maruw@ya.ru>
2024-01-22 12:29:16 +0100

    solve challenge

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ queryInput.addEventListener("input", (event) => {
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  const foundCountries = countries.filter((country) =>
+    country.name.startsWith(searchString)
+  );
 
-  if (foundCountry) {
-    const countryElement = Country(foundCountry);
-    container.append(countryElement);
+  if (foundCountries) {
+    foundCountries.map((foundCountry) => {
+      const countryElement = Country(foundCountry);
+      container.append(countryElement);
+    });
   }
 });
