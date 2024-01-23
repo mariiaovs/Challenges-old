f8c76cd6dfa6cddf8b93a20041efdc5248b5fd77
Mariia Posselt <maruw@ya.ru>
2024-01-23 10:21:31 +0100

    solve challenge

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(response);
+  console.log(data);
+  console.log(data.count);
+  for (const result of data.results) {
+    console.log(result.name);
+  }
+  console.log(data.results[2].eye_color);
+}
 
 fetchData();
