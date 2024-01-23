31fbdf4fbe69ca02f7b944295fc433bdea836e4e
Mariia Posselt <maruw@ya.ru>
2024-01-23 10:37:34 +0100

    solve challenge

--- js/index.js
+++ js/index.js
@@ -41,6 +41,11 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.dev/api/people");
+  const data = await response.json();
+  data.results.forEach((result) => {
+    const card = Card(result);
+    renderElement(card);
+  });
 }
